bc0c2c2994a9df7c54cadce60e25357efe3bc4c5
Giorgi Natroshvili <natroshvilig1999@gmail.com>
2019-09-27 15:47:35 +0400

    Added categories

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,12 +1,19 @@
 package net.kzn.onlineshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.kzn.shoppingbackend.dao.CategoryDAO;
+
 @Controller
 public class PageController {
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
+	
+	
 
 	//karoche es get requestebis pontebia 
 	//xoda unda yvela pagestvis cal calke get req dawero ra
@@ -14,6 +21,10 @@ public class PageController {
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
+		
+		//passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		
 		mv.addObject("userClickHome", true);
 		return mv;
 	}
